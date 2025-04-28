dec50d4844f64c36025fce94f0df2989fc821e3a
Abdulrahman Awwad <111631238+AbdoAwwad93@users.noreply.github.com>
2025-04-28 07:38:32 +0300

    som fixes

--- Hubs/BookingHub.cs
+++ Hubs/BookingHub.cs
@@ -4,6 +4,5 @@ namespace RailRoads_And_Routes.Hubs
 {
     public class BookingHub : Hub
     {
-        // You can add methods later if you want, but for now we just use it to broadcast seat updates
     }
 }
